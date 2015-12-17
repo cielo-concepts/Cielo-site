afe900cfa00812f0dfcc99e5bb573c21458601fe
Kianosh Pourian <kianoshp@gmail.com>
2015-12-17 14:01:18 -0500

    Added other projects

--- es6/data/portfolio.js
+++ es6/data/portfolio.js
@@ -46,22 +46,201 @@ let portfolio = {
         "url": "/portfolio/academy-sports"
       }
     }
-
   },
   "complete-eye-care": {
-
+    "titleImage": {
+      "src": "../img/clients/complete-eye-care/complete-eye-care-title-image.png",
+      "label": "Complete Eye Care of Texas",
+      "subLabel": "Small Business Website"
+    },
+    "projectTitle": "Website Design and Development",
+    "topParagraph": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
+    "client": {
+      "name": "Complete Eye Care of Texas",
+      "category": "Development, Design",
+      "url": "http://www.completefamilyeyecaretexas.com/"
+    },
+    "images": ["../img/clients/complete-eye-care/complete-eye-care-home.png", "../img/clients/complete-eye-care/complete-eye-care-about-us.png", "../img/clients/complete-eye-care/complete-eye-care-office-tour.png"],
+    "paragraph1": "<strong>Problem to Solve</strong><br/>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. <br/><strong> How We Did It</strong><br/>Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.",
+    "paragraph2": "Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?",
+    "technology": ["Design", "3rd party integration", "Sass CSS"],
+    "relatedItems": [
+      {
+        "image": "../img/projects/academy-small.png",
+        "url": "/portfolio/academy-sports",
+        "label": "Academy Sports Ecommerce",
+        "subLabel": "Development"
+      },
+      {
+        "image": "../img/projects/hbr-org-small.png",
+        "url": "/portfolio/hbr",
+        "label": "Harvard Business Review Redesign",
+        "subLabel": "Development"
+      },
+      {
+        "image": "../img/projects/perkin-elmer.png",
+        "url": "/portfolio/perkin-elmer",
+        "label": "Perkin Elmer \"Elements\"",
+        "subLabel": "Development, Design"
+      }
+    ],
+    "navigation": {
+      "next": {
+        "label": "Optum One / Humedica",
+        "url": "/portfolio/optum-one"
+      },
+      "prev": {
+        "label": "Academy Sports",
+        "url": "/portfolio/academy-sports"
+      }
+    }
   },
   "humedica": {
 
   },
   "perkin-elmer": {
-
+    "titleImage": {
+      "src": "../img/clients/perkin-elmer/perkin-elmer-responsive.png",
+      "label": "Perkin Elmer \"Elements\"",
+      "subLabel": "Re-Architecture of the UI Stack"
+    },
+    "projectTitle": "Perkin Elmer \"Elements\" Re-Architecture",
+    "topParagraph": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
+    "client": {
+      "name": "Perkin Elmer",
+      "category": "Development, Design",
+      "url": "http://elements.perkinElmer.com"
+    },
+    "images": ["../img/clients/perkin-elmer/perkin-elmer-home.png", "../img/clients/perkin-elmer/perkin-elmer-landing.png"],
+    "paragraph1": "<strong>Problem to Solve</strong><br/>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. <br/><strong> How We Did It</strong><br/>Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.",
+    "paragraph2": "Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?",
+    "technology": ["ReactJS", "Flux Pattern", "Gulp", "Pattern Lab", "Sass CSS"],
+    "relatedItems": [
+      {
+        "image": "../img/projects/academy-small.png",
+        "url": "/portfolio/academy-sports",
+        "label": "Academy Sports Ecommerce",
+        "subLabel": "Development"
+      },
+      {
+        "image": "../img/projects/eye-care-small.png",
+        "url": "/portfolio/complete-eye-care",
+        "label": "Complete Family Eye Care of Texas",
+        "subLabel": "Development, Design"
+      },
+      {
+        "image": "../img/projects/hbr-org-small.png",
+        "url": "/portfolio/hbr",
+        "label": "Harvard Business Review Redesign",
+        "subLabel": "Development"
+      }
+    ],
+    "navigation": {
+      "next": {
+        "label": "Complete Eye Care of Texas",
+        "url": "/portfolio/complete-eye-care"
+      },
+      "prev": {
+        "label": "Harvard Business Review",
+        "url": "/portfolio/hbr"
+      }
+    }
   },
   "morrisons-auto-rite": {
-
+    "titleImage": {
+      "src": "../img/clients/morrisons-auto-rite/morrisons-title-image.png",
+      "label": "Morrison's Auto Rite",
+      "subLabel": "Improvement to existing site"
+    },
+    "projectTitle": "Morrison's Auto Rite Website",
+    "topParagraph": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
+    "client": {
+      "name": "Morrison's Auto Rite",
+      "category": "Development, Design",
+      "url": "http://morrisonsautorite.com"
+    },
+    "images": ["../img/clients/morrisons-auto-rite/morrisons-home.png", "../img/clients/morrisons-auto-rite/morrisons-about-us.png"],
+    "paragraph1": "<strong>Problem to Solve</strong><br/>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. <br/><strong> How We Did It</strong><br/>Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.",
+    "paragraph2": "Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?",
+    "technology": ["Design", "Wordpress", "Responsive Design"],
+    "relatedItems": [
+      {
+        "image": "../img/projects/academy-small.png",
+        "url": "/portfolio/academy-sports",
+        "label": "Academy Sports Ecommerce",
+        "subLabel": "Development"
+      },
+      {
+        "image": "../img/projects/eye-care-small.png",
+        "url": "/portfolio/complete-eye-care",
+        "label": "Complete Family Eye Care of Texas",
+        "subLabel": "Development, Design"
+      },
+      {
+        "image": "../img/projects/hbr-org-small.png",
+        "url": "/portfolio/hbr",
+        "label": "Harvard Business Review Redesign",
+        "subLabel": "Development"
+      }
+    ],
+    "navigation": {
+      "next": {
+        "label": "Perkin Elmer \"Elements\"",
+        "url": "/portfolio/perkin-elmer"
+      },
+      "prev": {
+        "label": "Harvard Business Review",
+        "url": "/portfolio/hbr"
+      }
+    }
   },
   "academy-sports": {
-
+    "titleImage": {
+      "src": "../img/clients/academy/academy-title-image.png",
+      "label": "Academy Sports E-Commerce",
+      "subLabel": "Migrating from Brochureware to E-Commerce"
+    },
+    "projectTitle": "Academy Sports E-Commerce",
+    "topParagraph": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
+    "client": {
+      "name": "Academy Sports",
+      "category": "Development",
+      "url": "http://academy.com"
+    },
+    "images": ["../img/clients/academy/academy-product-page.png", "../img/clients/academy/academy-checkout-shipping.png", "../img/clients/academy/academy-checkout-shipping-review.png"],
+    "paragraph1": "<strong>Problem to Solve</strong><br/>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. <br/><strong> How We Did It</strong><br/>Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.",
+    "paragraph2": "Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?",
+    "technology": ["E-Commerce", "Development", "User Interface Development"],
+    "relatedItems": [
+      {
+        "image": "../img/projects/academy-small.png",
+        "url": "/portfolio/academy-sports",
+        "label": "Academy Sports Ecommerce",
+        "subLabel": "Development"
+      },
+      {
+        "image": "../img/projects/eye-care-small.png",
+        "url": "/portfolio/complete-eye-care",
+        "label": "Complete Family Eye Care of Texas",
+        "subLabel": "Development, Design"
+      },
+      {
+        "image": "../img/projects/hbr-org-small.png",
+        "url": "/portfolio/hbr",
+        "label": "Harvard Business Review Redesign",
+        "subLabel": "Development"
+      }
+    ],
+    "navigation": {
+      "next": {
+        "label": "Perkin Elmer \"Elements\"",
+        "url": "/portfolio/perkin-elmer"
+      },
+      "prev": {
+        "label": "Harvard Business Review",
+        "url": "/portfolio/hbr"
+      }
+    }
   },
   "pattern-lab-course": {
 
