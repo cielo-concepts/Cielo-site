229666b815d5432fbab1ea45e6cbc9369a7931a2
kianoshp <kianoshp@gmail.com>
2015-12-21 13:24:16 -0500

    Added the latest landing page elements

--- es6/data/portfolio.js
+++ es6/data/portfolio.js
@@ -6,15 +6,15 @@ let portfolio = {
       "subLabel": "2014 Redesign and Development Project"
     },
     "projectTitle": "HBR Redesign and Development",
-    "topParagraph": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
+    "topParagraph": "The Harvard Business Review (HBR) team were looking to adopt new technologies for both the front end and back end that were not only designed to push the capabilities of the product forward, but chosen in close collaboration with business stakeholders with a deep understanding of where the business is heading. HBR's goals are similar to any other magazine/publishing/media company, to grow the business by creating and retaining subscribers and simultaneously meet the changing needs of digital advertising as print revenues potentially decline. As the primary place where more and more people and clients engage with HBR, HBR.org has become the center of the business strategy.",
     "client": {
       "name": "Harvard Business Review",
       "category": "Development",
       "url": "http://hbr.org"
     },
     "images": ["../img/clients/hbr/hbr-home-page.png", "../img/clients/hbr/hbr-store.png", "../img/clients/hbr/hbr-my-library.png"],
-    "paragraph1": "<strong>Problem to Solve</strong><br/>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. <br/><strong> How We Did It</strong><br/>Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.",
-    "paragraph2": "Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?",
+    "paragraph1": "<strong>Problem to Solve</strong><br/>The major goal of this project was to update the hbr.org site with technology, user experience, user interface design that will bring the site into a new toolset and also this technology toolset should be extensible.<br/><br/><strong>How we did it</strong><br/>After <a href=\"http://www.hugeinc.com/\">HUGE</a> created the new design for the site, in collaboration with the internal development team and <a href=\"http://bocoup.com\">Bocoup</a> we implemented Pattern Lab and a new custom JavaScript framework.",
+    "paragraph2": "Our goal was to create a modular component based architecture that can fulfill the requirements of the project (extensible technology stack). <a href=\"http://patternlab.io\">Pattern Lab</a> allowed for the translation of the design into code using \"Atomic Design\" principles. The modular development was also extended to the JavaScript layer of the site, allowing for component based development that will make the site ready for future implementation of patterns and libararies like React.",
     "technology": ["Custom JavaScript Framework", "Modular Components", "Pattern Lab", "Sass CSS", "E-Commerce"],
     "relatedItems": [
       {
@@ -40,30 +40,26 @@ let portfolio = {
       "next": {
         "label": "Perkin Elmer \"Elements\"",
         "url": "/portfolio/perkin-elmer"
-      },
-      "prev": {
-        "label": "Academy Sports",
-        "url": "/portfolio/academy-sports"
       }
     }
   },
-  "complete-eye-care": {
+  "humedica": {
     "titleImage": {
-      "src": "../img/clients/complete-eye-care/complete-eye-care-title-image.png",
-      "label": "Complete Eye Care of Texas",
-      "subLabel": "Small Business Website"
+      "src": "../img/parallax.png",
+      "label": "Humedica Architecture Migration",
+      "subLabel": "Migration from Flex frameowrk to React and Redux"
     },
-    "projectTitle": "Website Design and Development",
-    "topParagraph": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
+    "projectTitle": "Humedica Migration",
+    "topParagraph": "Optum One Analytics (“Analytics”) is an informatics system for patient care allowing those in the medical field to query a large data set to pinpoint areas of interest through data tables, charting and visualization. <blockquote>Optum One can help you pinpoint opportunities to improve health care before, during and after patients engage the healthcare system. This cloud-based platform uses integrated data to manage patient populations in a value-based world</blockquote>",
     "client": {
-      "name": "Complete Eye Care of Texas",
-      "category": "Development, Design",
-      "url": "http://www.completefamilyeyecaretexas.com/"
+      "name": "Humedica / Optum",
+      "category": "Development",
+      "url": "http://www.humedica.com/"
     },
-    "images": ["../img/clients/complete-eye-care/complete-eye-care-home.png", "../img/clients/complete-eye-care/complete-eye-care-about-us.png", "../img/clients/complete-eye-care/complete-eye-care-office-tour.png"],
-    "paragraph1": "<strong>Problem to Solve</strong><br/>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. <br/><strong> How We Did It</strong><br/>Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.",
-    "paragraph2": "Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?",
-    "technology": ["Design", "3rd party integration", "Sass CSS"],
+    "images": ["../img/b-1.png", "../img/b-1.png"],
+    "paragraph1": "<strong>Problem to Solve</strong><br/>Our goals and objectives are to: <ul><li>Modernize the technology stack by moving from a Adobe Flex based application to a modern JavaScript application.</li><li>Improve the development cycle and workflow with the introduction of a new development and technology environment. This presents an opportunity to integrate new tools improve the current workflow.</li><li>Promote continuous development and integration.</li><li>Lay the groundwork for future mobile integration.</li></ul>",
+    "paragraph2": "<strong>How we did it</strong><br/>After our initial discovery phase, our recommendation was to rebuild the site in a modular fashion. \"Componentization\" or modular development is the main practice in achieving the goals of this project. Although \"componentization\" is a novel word, it has a meaning in the software development field: Simply put, software \"componentization\" occurs when one breaks down a complex software system down into smaller, easily identifiable pieces that have well-defined interfaces or APIs. To follow this principle, we implemented Pattern Lab for the HTML and CSS and ReactJS and Redux for the JavaScript layer. This approach was applied to an isolated portion of the site as a proof of concept.",
+    "technology": ["ReactJS", "Redux (Flux framework)", "Modular Components", "Pattern Lab", "Sass CSS"],
     "relatedItems": [
       {
         "image": "../img/projects/academy-small.png",
@@ -72,10 +68,10 @@ let portfolio = {
         "subLabel": "Development"
       },
       {
-        "image": "../img/projects/hbr-org-small.png",
-        "url": "/portfolio/hbr",
-        "label": "Harvard Business Review Redesign",
-        "subLabel": "Development"
+        "image": "../img/projects/eye-care-small.png",
+        "url": "/portfolio/complete-eye-care",
+        "label": "Complete Family Eye Care of Texas",
+        "subLabel": "Development, Design"
       },
       {
         "image": "../img/projects/perkin-elmer.png",
@@ -86,17 +82,10 @@ let portfolio = {
     ],
     "navigation": {
       "next": {
-        "label": "Optum One / Humedica",
-        "url": "/portfolio/optum-one"
-      },
-      "prev": {
-        "label": "Academy Sports",
-        "url": "/portfolio/academy-sports"
+        "label": "Perkin Elmer \"Elements\"",
+        "url": "/portfolio/perkin-elmer"
       }
     }
-  },
-  "humedica": {
-
   },
   "perkin-elmer": {
     "titleImage": {
@@ -146,6 +135,54 @@ let portfolio = {
       }
     }
   },
+  "complete-eye-care": {
+    "titleImage": {
+      "src": "../img/clients/complete-eye-care/complete-eye-care-title-image.png",
+      "label": "Complete Eye Care of Texas",
+      "subLabel": "Small Business Website"
+    },
+    "projectTitle": "Website Design and Development",
+    "topParagraph": "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
+    "client": {
+      "name": "Complete Eye Care of Texas",
+      "category": "Development, Design",
+      "url": "http://www.completefamilyeyecaretexas.com/"
+    },
+    "images": ["../img/clients/complete-eye-care/complete-eye-care-home.png", "../img/clients/complete-eye-care/complete-eye-care-about-us.png", "../img/clients/complete-eye-care/complete-eye-care-office-tour.png"],
+    "paragraph1": "<strong>Problem to Solve</strong><br/>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. <br/><strong> How We Did It</strong><br/>Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.",
+    "paragraph2": "Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?",
+    "technology": ["Design", "3rd party integration", "Sass CSS"],
+    "relatedItems": [
+      {
+        "image": "../img/projects/academy-small.png",
+        "url": "/portfolio/academy-sports",
+        "label": "Academy Sports Ecommerce",
+        "subLabel": "Development"
+      },
+      {
+        "image": "../img/projects/hbr-org-small.png",
+        "url": "/portfolio/hbr",
+        "label": "Harvard Business Review Redesign",
+        "subLabel": "Development"
+      },
+      {
+        "image": "../img/projects/perkin-elmer.png",
+        "url": "/portfolio/perkin-elmer",
+        "label": "Perkin Elmer \"Elements\"",
+        "subLabel": "Development, Design"
+      }
+    ],
+    "navigation": {
+      "next": {
+        "label": "Morrison's Auto Rite",
+        "url": "/portfolio/morrisons-auto-rite"
+      },
+      "prev": {
+        "label": "Perkin Elmer \"Elements\"",
+        "url": "/portfolio/perkin-elmer"
+      }
+    }
+  },
   "morrisons-auto-rite": {
     "titleImage": {
       "src": "../img/clients/morrisons-auto-rite/morrisons-title-image.png",
@@ -185,12 +222,12 @@ let portfolio = {
     ],
     "navigation": {
       "next": {
-        "label": "Perkin Elmer \"Elements\"",
-        "url": "/portfolio/perkin-elmer"
+        "label": "Academy Sports E-Commerce",
+        "url": "/portfolio/academy-sports"
       },
       "prev": {
-        "label": "Harvard Business Review",
-        "url": "/portfolio/hbr"
+        "label": "Complete Eye Care of Texas",
+        "url": "/portfolio/complete-eye-care"
       }
     }
   },
@@ -232,13 +269,9 @@ let portfolio = {
       }
     ],
     "navigation": {
-      "next": {
-        "label": "Perkin Elmer \"Elements\"",
-        "url": "/portfolio/perkin-elmer"
-      },
       "prev": {
-        "label": "Harvard Business Review",
-        "url": "/portfolio/hbr"
+        "label": "Morrison's Auto Rite",
+        "url": "/portfolio/morrisons-auto-rite"
       }
     }
   },
